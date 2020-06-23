0d16ddccf3f559be6345d4c01f4ec7c1f4b7704c
crisstels <natalie-hasselmann@web.de>
2020-06-23 10:38:18 +0200

    added calculator with plus

--- HelloApp.py
+++ HelloApp.py
@@ -1,1 +1,17 @@
+
+from flask import request
 from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def calculator():
+    a = request.args.get('a')
+    b = request.args.get('b')
+    return render_template('index.html', sum1=a, sum2=b)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
