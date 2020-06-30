6b2541a827cbd861a81cb04ccd1cf644eecec40c
crisstels <natalie-hasselmann@web.de>
2020-06-30 12:07:44 +0200

    test

--- HelloApp.py
+++ HelloApp.py
@@ -20,4 +20,4 @@ class Calculator(Resource):
 api.add_resource(Calculator, "/sum")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
--- test_HelloApp.py
+++ test_HelloApp.py
@@ -1,6 +1,7 @@
 ''' Unit test for HelloApp.py application'''
 import json
 import unittest
+from unittest.mock import patch
 
 import requests
 
@@ -9,28 +10,28 @@ from HelloApp import app
 
 class TestFlask(unittest.TestCase):
 
-    # # check response
-    # def test_index(self):
-    #     tester = app.test_client(self)
-    #     response = tester.get("/sum")
-    #     statuscode = response.status_code
-    #     self.assertEqual(statuscode, 405)
-
     # check if content return is application/json
     def test_content(self):
         tester = app.test_client(self)
         response = tester.get("/sum")
         self.assertEqual(response.content_type, "application/json")
 
-    # check for data returned
+    # checks returned data
     def test_post(self, mock_post):
         info = {"num1": 1, "num2": 1}
-        resp = requests.post("http://127.0.0.1:5000/", data=json.dumps(info),
-                             headers={'Content-Type': 'application/json'})
-        mock_post.assert_called_with("http://127.0.0.1:5000/", data=json.dumps(info),
-                                     headers={'Content-Type': 'application/json'})
+        resp = requests.post("http://127.0.01:5000/sum", data=json.dumps(info),
+                             headers={'Content type': 'application/json'})
+        mock_post.assert_called_with("http://127.0.0.1:5000/sum", data=json.dumps(info),
+                                     headers={'Content type': 'application/json'})
+
+    # def test_Calculator(self):
+    #
+    #     calculator = Calculator()
+    #     output = {'sum': 7}
+    #
+    #     result = calculator.post()
+    #     self.assertEqual(output, result)
 
 
 if __name__ == "__main__":
-    if __name__ == '__main__':
-        unittest.main()
+    unittest.test_post(patch)
