89613e04f9ced3485128f1366bb401c5aad5769e
crisstels <natalie-hasselmann@web.de>
2020-07-02 13:25:06 +0200

    adjust exception raise

--- test_calculator.py
+++ test_calculator.py
@@ -34,7 +34,7 @@ class TestCalculator(unittest.TestCase):
         calculator = Calculator()
         input = {'num1': None, 'num2': None}
 
-        with self.assertRaises(TypeError) as cm:
+        with self.assertRaises(TypeError):
             calculator.post(input)
 
 
