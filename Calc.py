150b4f342eebe79246376109b41e266339edbfb6
crisstels <natalie-hasselmann@web.de>
2020-07-02 13:30:00 +0200

    raise exception if input in not from type integer

--- Calc.py
+++ Calc.py
@@ -1,4 +1,7 @@
 class Calculator():
     '''Submits two numbers and returns the sum.'''
     def post(self, numbers):
+        if not (type(numbers['num1']) is int) and not (type(numbers['num2'] is int)):
+            raise TypeError("Only integers are allowed!")
+
         return {'sum': numbers['num1'] + numbers['num2']}
